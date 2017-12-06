85f129d16e69b56e7e40321c0aee92776e371eaf
dy_li <752860629@qq.com>
2017-12-06 17:57:30 +0800

    add Bmob SDK

---
+++ BmobDemo/BmobDemo/Define/BMDefine.swift
@@ -0,0 +1,17 @@
+//
+//  BMDefine.swift
+//  BmobDemo
+//
+//  Created by 李冬阳 on 2017/12/06.
+//  Copyright © 2017年 lidongyang. All rights reserved.
+//
+
+import Foundation
+
+
+class BMDefine: NSObject {
+  /** Bmob Application ID*/
+  static let BMOB_APPLICATION_ID: String  = "436146b65c63f8fc057653eb0e554436"
+  /** Bmob Resr API Key*/
+  static let BMOB_REST_API_KEY: String    = "a110374055564ea5a86339b063500b2a"
+}
---
+++ BmobDemo/Pods/Target Support Files/Pods-BmobDemo/Pods-BmobDemo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BmobDemo : NSObject
+@end
+@implementation PodsDummy_Pods_BmobDemo
+@end
